1006ada9734aa8f6e97f3f835ebf7246ca5a358a
Mislav Srecec <mislav.srecec3@gmail.com>
2020-09-15 19:22:06 +0200

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -10,6 +10,7 @@ import {
   BreadcrumbItem,
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderMenuItem({ dish, onClick }) {
   return (
@@ -27,7 +28,7 @@ function RenderMenuItem({ dish, onClick }) {
 }
 
 const Menu = (props) => {
-  const menu = props.dishes.map((dish) => {
+  const menu = props.dishes.dishes.map((dish) => {
     // console.log(`Rendering menu item id: ${dish.id}`);
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1">
@@ -36,25 +37,41 @@ const Menu = (props) => {
     );
   });
 
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/home">Home</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>
-            Menu
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>Menu</h3>
-          <hr />
+  if (props.dishes.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
         </div>
       </div>
-      <div className="row">{menu}</div>
-    </div>
-  );
+    );
+  } else if (props.dishes.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.dishes.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Menu</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">{menu}</div>
+      </div>
+    );
+  }
 };
 
 export default Menu;
